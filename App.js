f27fcb9a343c4d8c2f0365feb256c20689b35590
MaisieGao <gao.shuy@northeastern.edu>
2022-09-19 14:33:49 -0700

    change app name

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+// if you want to use this function, you have to export it
 export default function App() {
+  const myapp = "string"
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on {myapp}!</Text>
       <StatusBar style="auto" />
     </View>
   );
