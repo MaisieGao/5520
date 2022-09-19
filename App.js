f1a793a1d0fdb6701d6b717b7c2225dc7540ee00
MaisieGao <gao.shuy@northeastern.edu>
2022-09-19 15:26:46 -0700

    add a header component and a textInput in app.js

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import Header from './components/Header';
 // if you want to use this function, you have to export it
 export default function App() {
-  const myapp = "string"
+  const name = "my app"
+  const [text, setText] = useState("")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on {myapp}!</Text>
+      <Header appName={name}/>
       <StatusBar style="auto" />
+      {/* another text area that shows text on the view */}
+      <Text>{text}</Text>
+      <TextInput
+        style={styles.input}
+      //if you don't type anything, it shows 
+        placeholder="type whatever"
+        // when text change, setText is called and set t to test
+        onChangeText={t => setText(t)}
+        // the input that shows in the textbar
+        value={text}
+      />
     </View>
   );
 }
@@ -18,4 +33,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
 });
