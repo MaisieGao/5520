de8af07b83172cd171d634d63e827891dc341bb9
MaisieGao <MaisieGao@MacBook-Air.local>
2022-10-17 15:44:00 -0700

    add everything

--- components/GoalDetail.js
+++ components/GoalDetail.js
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function GoalDetail({route}) {
+export default function GoalDetail({route,navigation}) {
   return (
     <View>
       <Text>You are viewing the details of {route.params.goalObject.text}</Text>
