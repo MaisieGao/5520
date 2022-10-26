b97c5e49326056793d12fde8378df673492ac336
MaisieGao <90473308+MaisieGao@users.noreply.github.com>
2022-10-26 16:07:24 -0700

    Delete helperForColor.js

--- helperForColor.js
+++
@@ -1,9 +0,0 @@
-export default {
-    tabActive:'#ffd700',
-    tabInactive:'#808080',
-    lightScreen: '#9575cd',
-    darkScreen: '#673ab7',
-    wordColor:'#fff',
-    pressButton:'#9370db',
-    plainScreen:'#ae92e2'
-  }
