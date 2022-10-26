4018932b916e30d14ddb7e25c7a1a30c13ccd814
shuyuegao <gao.shuy@northeastern.edu>
2022-10-26 16:09:04 -0700

    delete some rows for firebase

--- firebase/firebase-setup.js
+++ firebase/firebase-setup.js
@@ -6,17 +6,11 @@ import {apiKey,authDomain,projectId,storageBucket,messagingSenderId,appId} from 
 
 const firebaseConfig = {
   apiKey: apiKey,
-
   authDomain: authDomain,
-
   projectId: projectId,
-
   storageBucket: storageBucket,
-
   messagingSenderId: messagingSenderId,
-
   appId: appId
-
 };
 
 
